9641d09be688cadd7de882ebee659e325d84a3fc
soham2002-btech <78204011+soham2002-btech@users.noreply.github.com>
2025-06-11 02:24:38 +0530

    Update github_mcp.py

--- github_mcp.py
+++ github_mcp.py
@@ -4,7 +4,6 @@ import logging
 from dotenv import load_dotenv
 import time
 
-# Configure logging
 logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
